20692a70f0c8f131222af88216d30797d9da6f91
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-01-03 21:56:16 +0100

    Set up server and client sides

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, underworld</h1>");
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+});
