767344fdd58a1a691318a0e8ee9a1cb526cde485
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-01-04 15:06:10 +0100

    Set up login page

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,18 +1,46 @@
 import Input from "../Input";
+import Button
+    from "../Button";
+import { useState } from "react";
 
 const LoginPage = () => {
+    const [formInputs, setFormInputs] = useState({
+        email: '',
+        password: ''
+    })
+    const handleChangeInputValue = e => {
+        setFormInputs({ formInputs, [e.target.name]: e.target.value });
+    }
+    const handleSubmitForm = e => {
+        e.preventDefault();
+    }
     return (
-        <>
-        <h1>LoginPage</h1>
-        <form>
-            <Input 
-            label="Name"
-            type="text"
-            placeholder="Ivan"
-            value="Ivan"
-            />
-        </form>
-        </>
+        <div className="login login-wrapper">
+            <div className="login-container">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmitForm}>
+                    <Input
+                        label="Email"
+                        name="email"
+                        type="email"
+                        placeholder="ivan@example.com"
+                        value={formInputs.email}
+                        onChange={handleChangeInputValue}
+                    />
+                    <Input
+                        label="Password"
+                        name="password"
+                        type="password"
+                        placeholder="******"
+                        value={formInputs.password}
+                        onChange={handleChangeInputValue}
+                    />
+                    <div className="btn-group">
+                        <Button design='default'>Login</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
