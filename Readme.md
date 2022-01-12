64b8dab43ecb3430c50528b51cc118447b3bc0c9
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-01-12 23:39:56 +0100

    Set up set-up for datatbase and added instructions to readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,20 @@
-# Authorization case with JSON-token saved in localStorage
+# Authorization with sessions and cookies
+
+## Install dependencies
+cd client
+npm i
+
+cd server
+npm i
+
+## Set up database
+Run in CL postgres content of the server/db-setup.sql
+
+## Set up .env
+Create inside server folder .env and add define inside:
+* PORT=5000
+* JWT_SECRET=[Secret for JWT generation]
+* PG_USER=postgres
+* PG_HOST=localhost
+* PG_PORT=[probably "5432"]
+* PG_DATABASE=auth_sessions_cookies
---
+++ server/db-setup.sql
@@ -0,0 +1,23 @@
+-- Create database
+CREATE DATABASE auth_sessions_cookies;
+
+--Go to database
+\c auth_sessions_cookies;
+
+-- Users schema
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NUll,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Add new user (Signup)
+INSERT INTO
+    users (name, email, password)
+VALUES
+    (
+        'Anna',
+        'anna@example.com',
+        'blakgsfbhk-fwkbafe-aebfhklb'
+    );
